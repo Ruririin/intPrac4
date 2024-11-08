a3060cf01ec5bae4162733a9c37e72009e130daf
Ruririin <danise.cano@yahoo.com>
2024-11-08 08:36:37 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace intPrac4
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(new DateTime(2002, 11, 30));
+            Console.WriteLine(person.Age);
+        }
+    }
+}
